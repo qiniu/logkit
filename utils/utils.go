a7851b28d9e2bb9b7b978a6101e2c5edf7e4a1a2
wonderflow <wonderflow.sun@gmail.com>
2017-09-12 15:34:16 +0800

    fix logkit

--- utils/utils.go
+++ utils/utils.go
@@ -135,8 +135,8 @@ type StatsError struct {
 type StatsInfo struct {
 	Errors    int64   `json:"errors"`
 	Success   int64   `json:"success"`
-	Speed     float64 `json:"speed,omitempty"`
-	Trend     string  `json:"trend,omitempty"`
+	Speed     float64 `json:"speed"`
+	Trend     string  `json:"trend"`
 	LastError string  `json:"last_error"`
 	Ftlag     int64   `json:"-"`
 }
