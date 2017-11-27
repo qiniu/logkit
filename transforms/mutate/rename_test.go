37f73d121823c8178491be01303a0605e2456442
ll <925783178@qq.com>
2017-11-27 17:02:08 +0800

    fix rename_test.go

--- transforms/mutate/rename_test.go
+++ transforms/mutate/rename_test.go
@@ -9,12 +9,21 @@ import (
 
 func TestRenameTransformer(t *testing.T) {
 	rename := &Rename{
+		Key: "a.ts",
+		NewKeyName:"@timestamp",
+	}
+	data, err := rename.Transform([]sender.Data{{"a":map[string]interface{}{"ts":"stamp1"}}, {"ts":"stamp2"}})
+	assert.NoError(t, err)
+	exp := []sender.Data{{"@timestamp":"stamp1","a":map[string]interface{}{}},{"ts":"stamp2"}}
+	assert.Equal(t, exp, data)
+	assert.Equal(t, rename.Stage(), transforms.StageAfterParser)
+	rename2 := &Rename{
 		Key: "ts",
 		NewKeyName:"@timestamp",
 	}
-	data, err := rename.Transform([]sender.Data{{"ts":"stamp1"}, {"ts":"stamp2"}})
+	data2, err := rename2.Transform([]sender.Data{{"ts":"stamp1"}, {"ts":"stamp2"}})
 	assert.NoError(t, err)
-	exp := []sender.Data{{"@timestamp":"stamp1"},{"@timestamp":"stamp2"}}
-	assert.Equal(t, exp, data)
-	assert.Equal(t, rename.Stage(), transforms.StageAfterParser)
+	exp2 := []sender.Data{{"@timestamp":"stamp1"},{"@timestamp":"stamp2"}}
+	assert.Equal(t, exp2, data2)
+	assert.Equal(t, rename2.Stage(), transforms.StageAfterParser)
 }
