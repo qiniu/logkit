96a7c3f146711a14ddeaebda00c86e234afcfaf9
zqz <zhangqingzhou@bonc.com.cn>
2017-11-30 15:12:10 +0800

    fix autofile.go and front

--- reader/autofile.go
+++ reader/autofile.go
@@ -1,65 +1,52 @@
 package reader
 
 import (
-	"io/ioutil"
-	"path"
+	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/qiniu/logkit/conf"
 )
 
-const (
-	winsign = "\\"
-	comsign = "/"
-)
-
-func NewFileAutoReader(conf conf.MapConf, meta *Meta, isFromWeb bool) (reader Reader, err error) {
-	logpath, err := conf.GetString(KeyLogPath)
-	bufSize, _ := conf.GetIntOr(KeyBufSize, defaultBufSize)
-	whence, _ := conf.GetStringOr(KeyWhence, WhenceOldest)
-	var fr FileReader
-	//windows path for replacement
-	logpath = strings.Replace(logpath, winsign, comsign, -1)
-	// for example: The path is "/usr/logkit/"
-	_, after := path.Split(logpath)
+func NewFileAutoReader(conf conf.MapConf, meta *Meta, isFromWeb bool, bufSize int, whence string, logpath string, fr FileReader) (reader Reader, err error) {
+	// for example: The path is "/usr/logkit/" or "F:\\user\\logkit\\" after==""
+	// for example: The path is "/usr/logkit" or "F:\\user\\logkit"after==logkit
+	_, after := filepath.Split(logpath)
 	if after == "" {
-		logpath = path.Dir(logpath)
+		logpath = filepath.Dir(logpath)
 	}
 	//path with * matching tailx mode
-	isSub := strings.Contains(logpath, "*")
-	if isSub == true {
+	matchTailx := strings.Contains(logpath, "*")
+	if matchTailx == true {
 		meta.mode = ModeTailx
 		expireDur, _ := conf.GetStringOr(KeyExpire, "24h")
 		stateIntervalDur, _ := conf.GetStringOr(KeyStatInterval, "3m")
 		maxOpenFiles, _ := conf.GetIntOr(KeyMaxOpenFiles, 256)
 		reader, err = NewMultiReader(meta, logpath, whence, expireDur, stateIntervalDur, maxOpenFiles)
 	} else {
-		//for "/usr/logkit" this path to make judgments
-		dirStr := path.Dir(logpath)
-		lastStr := path.Base(logpath)
-		files, _ := ioutil.ReadDir(dirStr)
-		for _, file := range files {
-			if file.Name() == lastStr {
-				if file.IsDir() {
-					meta.mode = ModeDir
-					ignoreHidden, _ := conf.GetBoolOr(KeyIgnoreHiddenFile, true)
-					ignoreFileSuffix, _ := conf.GetStringListOr(KeyIgnoreFileSuffix, defaultIgnoreFileSuffix)
-					validFilesRegex, _ := conf.GetStringOr(KeyValidFilePattern, "*")
-					fr, err = NewSeqFile(meta, logpath, ignoreHidden, ignoreFileSuffix, validFilesRegex, whence)
-					if err != nil {
-						return
-					}
-					reader, err = NewReaderSize(fr, meta, bufSize)
-				} else {
-					meta.mode = ModeFile
-					fr, err = NewSingleFile(meta, logpath, whence, isFromWeb)
-					if err != nil {
-						return
-					}
-					reader, err = NewReaderSize(fr, meta, bufSize)
-				}
-				break
+		//for logpath this path to make judgments
+		fileInfo, errStat := os.Stat(logpath)
+		if errStat != nil {
+			err = errStat
+			return
+		}
+		if fileInfo.IsDir() == true {
+			meta.mode = ModeDir
+			ignoreHidden, _ := conf.GetBoolOr(KeyIgnoreHiddenFile, true)
+			ignoreFileSuffix, _ := conf.GetStringListOr(KeyIgnoreFileSuffix, defaultIgnoreFileSuffix)
+			validFilesRegex, _ := conf.GetStringOr(KeyValidFilePattern, "*")
+			fr, err = NewSeqFile(meta, logpath, ignoreHidden, ignoreFileSuffix, validFilesRegex, whence)
+			if err != nil {
+				return
 			}
+			reader, err = NewReaderSize(fr, meta, bufSize)
+		} else {
+			meta.mode = ModeFile
+			fr, err = NewSingleFile(meta, logpath, whence, isFromWeb)
+			if err != nil {
+				return
+			}
+			reader, err = NewReaderSize(fr, meta, bufSize)
 		}
 	}
 	return
