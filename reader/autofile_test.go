90b29d508309483ca11db8d296394e97a181c5f3
zqz <zhangqingzhou@bonc.com.cn>
2017-12-04 14:35:24 +0800

    test file fix

--- reader/autofile_test.go
+++ reader/autofile_test.go
@@ -8,21 +8,24 @@ import (
 )
 
 func Test_matchModeDir(t *testing.T) {
-	var logpathdir string = "/usr/"
+	var logpathdir string = os.TempDir() + "logkit"
+	os.Mkdir(logpathdir,defaultFilePerm)
 	modedir, err := matchMode(logpathdir)
 	assert.NoError(t, err)
 	assert.Equal(t, modedir, ModeDir)
 }
 
 func Test_matchModeFile(t *testing.T){
-	var logpathfile string = "/usr/logkit.log"
+	var logpathfile string = os.TempDir() + "logkit.log"
+	os.Create(logpathfile)
 	modefile, err2 := matchMode(logpathfile)
 	assert.NoError(t, err2)
 	assert.Equal(t, modefile, ModeFile)
 }
 
 func Test_matchModeTailx(t *testing.T){
-	var logpathtailx string = "/usr/*/*.log"
+	var logpathtailx string = "/logkit/*/*.log"
+	os.Create(os.TempDir() + "logkit/1/logkit.log")
 	modetailx, err3 := matchMode(logpathtailx)
 	assert.NoError(t, err3)
 	assert.Equal(t, modetailx, ModeTailx)
