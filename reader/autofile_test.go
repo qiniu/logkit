564c330bc5bf01153c6a8d040435d5bb05a19f7e
zqz <zhangqingzhou@bonc.com.cn>
2017-12-04 13:15:37 +0800

    fix autofile_test.go

--- reader/autofile_test.go
+++ reader/autofile_test.go
@@ -2,23 +2,38 @@ package reader
 
 import (
 	"testing"
+	"os"
 	
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_matchMode(t *testing.T) {
-	var logpathdir string = "F:\\apache-tomcat-6.0.18\\log\\"
+func Test_matchModeDir(t *testing.T) {
+	var logpathdir string = "/usr/logkit"
+	os.Mkdir(logpathdir,defaultDirPerm)
+	defer remove(logpathdir)
 	modedir, err := matchMode(logpathdir)
 	assert.NoError(t, err)
 	assert.Equal(t, modedir, ModeDir)
-	
-	var logpathfile string = "F:\\apache-tomcat-6.0.18\\log\\catalina.log"
+}
+
+func Test_matchModeFile(t *testing.T){
+	var logpathfile string = "/usr/logkit/logkit.log"
+	os.Create(logpathfile)
+	defer remove(logpathfile)
 	modefile, err2 := matchMode(logpathfile)
 	assert.NoError(t, err2)
 	assert.Equal(t, modefile, ModeFile)
-	
-	var logpathtailx string = "F:\\apache-tomcat-6.0.18\\*\\*.log"
-	modetailx, err3 := matchMode(logpathfile)
+}
+
+func Test_matchModeTailx(t *testing.T){
+	var logpathtailx string = "/usr/logkit/*/*.log"
+	os.Create("/usr/logkit/1/logkit.log")
+	defer remove("/usr/logkit/1/logkit.log")
+	modetailx, err3 := matchMode(logpathtailx)
 	assert.NoError(t, err3)
 	assert.Equal(t, modetailx, ModeTailx)
+}
+
+func remove(logpath string) {
+	os.RemoveAll(logpath)
 }
