197bbc5260bcde0a302700be8a10cbfa12268141
wonderflow <wonderflow.sun@gmail.com>
2018-03-22 18:06:07 +0800

    fix pandorakey_convert

--- transforms/mutate/pandorakey_convert_test.go
+++ transforms/mutate/pandorakey_convert_test.go
@@ -16,7 +16,7 @@ func TestDeepconvertkey(t *testing.T) {
 	exp := []Data{{"ts_ts2": "stamp1"}, {"ts_tes2_1_2": "stamp2"}}
 	assert.Equal(t, exp, got)
 
-	data = []Data{{"ts。ts2": map[string]interface{}{"xs1_2s.xs.1": 1, "a.xs.1": 2}}, {"ts- ": "stamp2"}}
+	data = []Data{{"ts。ts2": map[string]interface{}{"_xs1_2s.xs.1": 1, "a.xs.1": 2}}, {"ts- ": "stamp2"}}
 	got, _ = pandoraConvert.Transform(data)
 	exp = []Data{{"ts_ts2": map[string]interface{}{"xs1_2s_xs_1": 1, "a_xs_1": 2}}, {"ts__": "stamp2"}}
 	assert.Equal(t, exp, got)
