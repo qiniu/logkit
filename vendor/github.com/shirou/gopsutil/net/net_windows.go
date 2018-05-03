0a15ec11bd0e6993c204f7f26831849338f0070a
chenwenhao <chenwenhao03@gmail.com>
2018-05-03 23:22:33 +0800

    make netstat work for windows

--- vendor/github.com/shirou/gopsutil/net/net_windows.go
+++ vendor/github.com/shirou/gopsutil/net/net_windows.go
@@ -5,8 +5,10 @@ package net
 import (
 	"context"
 	"errors"
+	"fmt"
 	"net"
 	"os"
+	"unsafe"
 
 	"github.com/shirou/gopsutil/internal/common"
 	"golang.org/x/sys/windows"
@@ -16,6 +18,7 @@ var (
 	modiphlpapi             = windows.NewLazyDLL("iphlpapi.dll")
 	procGetExtendedTCPTable = modiphlpapi.NewProc("GetExtendedTcpTable")
 	procGetExtendedUDPTable = modiphlpapi.NewProc("GetExtendedUdpTable")
+	procGetTcpTable         = modiphlpapi.NewProc("GetTcpTable")
 )
 
 const (
@@ -29,6 +32,59 @@ const (
 	TCPTableOwnerModuleConnections
 	TCPTableOwnerModuleAll
 )
+const ANY_SIZE = 100
+
+type DWORD uint32
+type MIB_TCPTABLE struct {
+	DwNumEntries DWORD
+	Table        [ANY_SIZE]MIB_TCPROW // TODO: pass array to dll func
+}
+type PMIB_TCPTABLE *MIB_TCPTABLE
+type MIB_TCPROW struct {
+	State        MIB_TCP_STATE
+	DwLocalAddr  DWORD
+	DwLocalPort  DWORD
+	DwRemoteAddr DWORD
+	DwRemotePort DWORD
+}
+type MIB_TCP_STATE int32
+
+const (
+	MIB_TCP_STATE_CLOSED     MIB_TCP_STATE = 1
+	MIB_TCP_STATE_LISTEN                   = 2
+	MIB_TCP_STATE_SYN_SENT                 = 3
+	MIB_TCP_STATE_SYN_RCVD                 = 4
+	MIB_TCP_STATE_ESTAB                    = 5
+	MIB_TCP_STATE_FIN_WAIT1                = 6
+	MIB_TCP_STATE_FIN_WAIT2                = 7
+	MIB_TCP_STATE_CLOSE_WAIT               = 8
+	MIB_TCP_STATE_CLOSING                  = 9
+	MIB_TCP_STATE_LAST_ACK                 = 10
+	MIB_TCP_STATE_TIME_WAIT                = 11
+	MIB_TCP_STATE_DELETE_TCB               = 12
+)
+
+var TcpStateMap = map[MIB_TCP_STATE]string{
+	MIB_TCP_STATE_CLOSED:     "CLOSE",
+	MIB_TCP_STATE_LISTEN:     "LISTEN",
+	MIB_TCP_STATE_SYN_SENT:   "SYN_SENT",
+	MIB_TCP_STATE_SYN_RCVD:   "SYN_RECV",
+	MIB_TCP_STATE_ESTAB:      "ESTABLISHED",
+	MIB_TCP_STATE_FIN_WAIT1:  "FIN_WAIT1",
+	MIB_TCP_STATE_FIN_WAIT2:  "FIN_WAIT2",
+	MIB_TCP_STATE_CLOSE_WAIT: "CLOSE_WAIT",
+	MIB_TCP_STATE_CLOSING:    "CLOSING",
+	MIB_TCP_STATE_LAST_ACK:   "LAST_ACK",
+	MIB_TCP_STATE_TIME_WAIT:  "TIME_WAIT",
+}
+
+func GetTcpTable(tcpTable PMIB_TCPTABLE, sizePointer *uint32, order bool) uint32 {
+	ret1, _, _ := procGetTcpTable.Call(
+		uintptr(unsafe.Pointer(tcpTable)),
+		uintptr(unsafe.Pointer(sizePointer)),
+		getUintptrFromBool(order))
+	return uint32(ret1)
+}
 
 func IOCounters(pernic bool) ([]IOCountersStat, error) {
 	return IOCountersWithContext(context.Background(), pernic)
@@ -85,8 +141,27 @@ func Connections(kind string) ([]ConnectionStat, error) {
 
 func ConnectionsWithContext(ctx context.Context, kind string) ([]ConnectionStat, error) {
 	var ret []ConnectionStat
-
-	return ret, common.ErrNotImplementedError
+	var tcpTable PMIB_TCPTABLE
+	var sizePointer uint32 = 0
+	resCode := GetTcpTable(tcpTable, &sizePointer, true)
+	//ERROR_INSUFFICIENT_BUFFER(122)
+	//The data area passed to a system call is too small.
+	if resCode != 122 {
+		return ret, fmt.Errorf("Call win func GetTcpTable failed, errCode: %v", resCode)
+	}
+	tcpTable = &MIB_TCPTABLE{}
+	// call twice
+	resCode = GetTcpTable(tcpTable, &sizePointer, true)
+	if resCode != 0 {
+		return ret, fmt.Errorf("Call win func GetTcpTable failed, errCode: %v", resCode)
+	}
+	for i := 0; DWORD(i) < tcpTable.DwNumEntries; i++ {
+		cs := ConnectionStat{
+			Status: TcpStateMap[tcpTable.Table[i].State],
+		}
+		ret = append(ret, cs)
+	}
+	return ret, nil
 }
 
 // Return a list of network connections opened returning at most `max`
@@ -117,4 +192,12 @@ func ProtoCounters(protocols []string) ([]ProtoCountersStat, error) {
 
 func ProtoCountersWithContext(ctx context.Context, protocols []string) ([]ProtoCountersStat, error) {
 	return nil, errors.New("NetProtoCounters not implemented for windows")
+}
+
+func getUintptrFromBool(b bool) uintptr {
+	if b {
+		return 1
+	} else {
+		return 0
+	}
 }
