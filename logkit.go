5b3d9f0d1733340915bfe3b97c70a2b3e4708c47
wonderflow <wonderflow.sun@gmail.com>
2018-06-11 20:22:53 +0800

    v1.5.1

--- logkit.go
+++ logkit.go
@@ -43,7 +43,7 @@ type Config struct {
 var conf Config
 
 const (
-	NextVersion       = "v1.5.0"
+	NextVersion       = "v1.5.1"
 	defaultReserveCnt = 5
 	defaultLogDir     = "./run"
 	defaultLogPattern = "*.log-*"
