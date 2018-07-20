66480a810f071a260e6a209bf13e2e2d3348933c
Sun Jianbo <wonderflow.sun@gmail.com>
2018-07-20 12:05:04 +0800

    fix error block (#618)
    * fix error block
    
    * add log

--- reader/kafka/kafka.go
+++ reader/kafka/kafka.go
@@ -81,7 +81,7 @@ func NewReader(meta *reader.Meta, conf conf.MapConf) (reader.Reader, error) {
 	config := consumergroup.NewConfig()
 	config.Zookeeper.Chroot = kr.ZookeeperChroot
 	config.Zookeeper.Timeout = kr.ZookeeperTimeout
-	config.Consumer.Return.Errors = true
+	//config.Consumer.Return.Errors = true  //怀疑有bug，会block 读取，先注释
 
 	/*********************  kafka offset *************************/
 	/* 这里设定的offset不影响原有的offset，因为kafka client会去获取   */
@@ -157,6 +157,8 @@ func (kr *Reader) ReadLine() (data string, err error) {
 				kr.curOffsets[msg.Topic] = tp
 			}
 			kr.statsLock.Unlock()
+		} else {
+			log.Debugf("runner[%v] Consumer read empty message: %v", kr.meta.RunnerName, msg)
 		}
 	case <-timer.C:
 	}
