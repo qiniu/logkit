e769c66bcefc75c8e15329ed0e3a58fef4290205
dingwenbiao <wbdingzx@163.com>
2019-07-09 10:08:57 +0800

    retry ci

--- sender/open_falcon/transfer.go
+++ sender/open_falcon/transfer.go
@@ -119,7 +119,7 @@ func (ts *TransferSender) Send(datas []Data) error {
 	timeStamp := time.Now().Unix()
 	for _, d := range datas {
 		for k, v := range d {
-			log.Infof("k = %s, v = %+v", k, v)
+			log.Debugf("k = %s, v = %+v", k, v)
 			if vmap, ok = v.(map[string]interface{}); ok {
 				for ik, iv := range vmap {
 					if tmpData, success := ts.converToTransferData(ik, iv, timeStamp); success {
