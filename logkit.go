cc360161c035fb4fb1b10f4443f933828c2112c2
redHJ <489534124@qq.com>
2020-05-19 16:52:38 +0800

    change version to 1.5.6

--- logkit.go
+++ logkit.go
@@ -45,7 +45,7 @@ type Config struct {
 var conf Config
 
 const (
-	NextVersion        = "v1.5.5"
+	NextVersion        = "v1.5.6"
 	defaultReserveCnt  = 5
 	defaultLogDir      = "./"
 	defaultLogPattern  = "*.log-*"
