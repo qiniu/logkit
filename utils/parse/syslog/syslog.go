b5bc85c035db99bc8773071b7a70e9ca833f76fb
王宇华 <suikalala@gmail.com>
2020-10-30 11:33:44 +0800

    [PDR-11312][feat(parser/transformer)]syslog severity/facility 对应到相应的字段，保留原始字段

--- utils/parse/syslog/syslog.go
+++ utils/parse/syslog/syslog.go
@@ -18,6 +18,44 @@ const (
 	DetectedLeftLog = iota
 )
 
+var MessageFacilities = map[int]string{
+	0:  "kernel messages",
+	1:  "user-level messages",
+	2:  "mail system",
+	3:  "system daemons",
+	4:  "security/authorization messages (note 1)",
+	5:  "messages generated internally by syslogd",
+	6:  "line printer subsystem",
+	7:  "network news subsystem",
+	8:  "UUCP subsystem",
+	9:  "clock daemon (note 2)",
+	10: "security/authorization messages (note 1)",
+	11: "FTP daemon",
+	12: "NTP subsystem",
+	13: "log audit (note 1)",
+	14: "log alert (note 1)",
+	15: "clock daemon (note 2)",
+	16: "local use 0  (local0)",
+	17: "local use 1  (local1)",
+	18: "local use 2  (local2)",
+	19: "local use 3  (local3)",
+	20: "local use 4  (local4)",
+	21: "local use 5  (local5)",
+	22: "local use 6  (local6)",
+	23: "local use 7  (local7)",
+}
+
+var MessageSeverities = map[int]string{
+	0: "Emergency: system is unusable",
+	1: "Alert: action must be taken immediately",
+	2: "Critical: critical conditions",
+	3: "Error: error conditions",
+	4: "Warning: warning conditions",
+	5: "Notice: normal but significant condition",
+	6: "Informational: informational messages",
+	7: "Debug: debug-level messages",
+}
+
 type LogParts map[string]interface{}
 
 type Parser interface {
